8d23596ad2a9e6969071a18a03c5a00cd8d70049
Vlad Bilanchuk <burning1848@gmail.com>
2017-03-24 09:06:59 +0200

    lab2-3

--- lab-1/index.php
+++ lab-1/index.php
@@ -11,21 +11,33 @@
 <body>
 
 <button id="start_button"> Let's start</button>
-<h1 id="head_text"> Registration form</h1>
+
+<div id="header">
+    <h1 id="head_text"> Registration form</h1>
+</div>
+
+<div id="left_block">
+    <img class="sabaka" src="img/img0.jpg" alt="XYI">
+</div>
+
 <div id="form_div">
     <form id="main_form" method="get" action="second.php">
         <div class="wrap-inner">
 
-
-
             <label for="user_name_input">Name:</label>
             <input id="user_name_input" type="text" required="required" name="user_name" />
 
+            <br/>
             <label for="user_password_input">Password:</label>
             <input id="user_password_input" type="password" required="required" name="user_password" />
 
+            <br/>
             <label for="program_name_input">Program name:</label>
             <input id="program_name_input" type="text" name="program_name" />
+            <br/>
+            <label for="email_input">Email:</label>
+            <input id="email_input" type="text" name="email_input"/>
+
             <div id="select_div">
                 <select id="select_method_select" onchange="set_method()" name="select_method_select">
 
@@ -37,9 +49,14 @@
 
             <input id="file_input" type="file" name="file_input" />
 
-            <button id="submit_button" type="submit"  name="submit_button"> Send </button>
+            <button id="submit_button" type="submit"  name="submit_button" onclick="return ValidateForm()"> Send </button>
         </div>
     </form>
 </div>
+
+<div id="right_block">
+    <p>right</p>
+</div>
+
 </body>
 </html>
--- lab-1/script.js
+++ lab-1/script.js
@@ -2,7 +2,6 @@
  * Created by lockistrike on 19.02.17.
  */
 
-
 function set_method() {
     main_form.setAttribute("method", select_method_select.value);
 
@@ -11,19 +10,100 @@ function set_method() {
 
 $(document).ready(function () {
     $(document).ready(function () {
-        $("#head_text").hide();
-        $("#main_form").hide();
+        $("#header").hide();
+        $("#form_div").hide();
+        $("#left_block").hide();
+        $("#right_block").hide();
     })
-})
+});
 
 $(document).ready(function () {
     $("#start_button").click(function () {
         $("#start_button").fadeOut(1000);
-        $("h1").fadeIn(2000);
-        $("form").fadeIn(2000);
-
-
-
+        $("#header").fadeIn(2000);
+        $("#form_div").fadeIn(2000);
+        $("#left_block").fadeIn(2000);
+        $("#right_block").fadeIn(2000);
+        $("#head_text").append(name);
     })
-})
+});
+
+
+let img_index = 0;
+let img_count = 5;
+
+function load_img() {
+    img_array = [];
+    let i;
+    for (i = 0; i < 5; i++) {
+        img_array[i] = "/img/img" + i + ".jpg";
+    }
+}
+
+$(document).ready(function () {
+    $(".sabaka").mouseenter(function () {
+        slider();
+        let color = Math.floor(Math.random() * 101 + Math.random() * 101);
+        //$("#submit_button").css("background", "rgb(" + color + "," + color/2 + ", " + color/3 +")");
+        let res = "rgb(" + color + ", " + Math.floor(color / 2) + ", " + Math.floor(color / 3) + ")";
+        $("#submit_button").css("background", res);
+    });
+});
+
+function slider() {
+    img_index++;
+    if (img_index >= img_count) {
+        img_index = 0;
+    }
+    let image = document.getElementsByClassName("sabaka")[0];
+    image.src = "img/img" + img_index + ".jpg";
+
+}
+
+let name;
+
+$(document).ready(function () {
+    alert("Hello, user! \n You eneter on " + document.URL);
+    name = prompt("Enter your name to welcome you: ");
+
+    /*if(name == ""){
+     document.getElementById("head_text").innerHTML = document.getElementById("head_text").textContent + "\n for annonymous";
+     }
+     else {
+     document.getElementById("head_text").innerHTML = document.getElementById("head_text").textContent + "\n for " + name;
+     }*/
+});
+
+
+function ValidateForm() {
+    return (document.getElementById("email_input").value.includes("@"));
+}
+
+(function ($) {
+    $.fn.jump = function () {
+        this.each(function () {
+            let img = $(".sabaka")[0];
+            if($(this).find($(".sabaka"))){
+                $(this).append(img);
+            }
+            // if($("#right_block").find($(".sabaka")).length  > 0){
+            //     $("#left_block").append(img);
+            // }
+            // else{
+            //     $("#right_block").append(img);
+            // }
+
+        });
+        return this;
+    };
+})(jQuery);  
+
+$(document).ready(function () {
+   $("div").click(function () {
+        $("div:hover").jump();
+   });
+});
+
+function jumper() {
+}
 
